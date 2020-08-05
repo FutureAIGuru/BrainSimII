9d64f38b5a7fe6dcbf5831feebbde99fc9c8dd57
Charles Simon <charles@futureai.guru>
2020-08-05 14:53:22 -0400

    Unrolll extra checkin

--- CppSpeedTest - Copy/Main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "NeuronArrayBase.h"
-#include "NeuronBase.h"
-#include <string>
-using namespace std;
-
-int main(int argc, char* argv[], char* envp[]);
-void outputElapsedTime(string msg);

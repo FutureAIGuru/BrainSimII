66cc0ac62288f313ce6f79ad2b4ae7e8a556a14b
Anthony Valente <anthony_valente@hotmail.com>
2024-04-12 13:59:52 +1000

    Replace "[Name]" with "Charles Simon".

--- BrainSimulator/Modules/ModuleFireOldest.cs
+++ BrainSimulator/Modules/ModuleFireOldest.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (c) [Name]. All rights reserved.  
+// Copyright (c) Charles Simon. All rights reserved.  
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 //  
 
